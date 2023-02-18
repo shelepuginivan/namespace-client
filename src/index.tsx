2813b55cce9aa03b4556c119402b5b5fc96acbcd
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 12:58:58 +0300

    added import variables.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@
 import { render } from 'solid-js/web'
 import App from './components/App'
 import './css/reset.css'
+import './css/variables.css'
 
 render(() => <App />, document.getElementById('root'))
