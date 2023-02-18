38bbddb167963a5238ad090cdaba45ff709f6750
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 14:18:51 +0300

    added modal in the page

--- src/ui/FilesList/FilesList.tsx
+++ src/ui/FilesList/FilesList.tsx
@@ -1,10 +1,13 @@
 import {JSX, ParentProps} from 'solid-js'
 import styles from './FilesList.module.css'
+import FileModal from "../../components/FileModal/FileModal";
 
 const FilesList = (props: ParentProps): JSX.Element => {
 	return (
 		<div class={styles.filesList}>
 			{props.children}
+
+			<FileModal/>
 		</div>
 	)
 }
