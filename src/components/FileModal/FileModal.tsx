7f3a7bc7297c35b202642b0adb1b3440f5467130
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 14:16:48 +0300

    added modal for opened file

---
+++ src/components/FileModal/FileModal.tsx
@@ -0,0 +1,44 @@
+import {createEffect, createSignal, JSX} from 'solid-js'
+import fsItemOpenedInModal from '../../store/fsItemOpenedInModal'
+import FileSystemItem from '../../utils/FileSystemItem'
+import styles from './FileModal.module.css'
+import ModalCloseButton from "../../ui/ModalCloseButton/ModalCloseButton";
+
+const FileModal = (): JSX.Element => {
+	const [getFsItemOpenedInModal, setFsItemOpenedInModal] = fsItemOpenedInModal
+	const [getItemName, setItemName] = createSignal<string>('')
+	const [getItemPath, setItemPath] = createSignal<string>('')
+
+	createEffect(() => {
+		if (getFsItemOpenedInModal() instanceof FileSystemItem) {
+			setItemName(getFsItemOpenedInModal().name)
+			setItemPath(getFsItemOpenedInModal().path)
+		}
+	})
+
+	const closeModal = () => {
+		setFsItemOpenedInModal(null)
+	}
+
+	return (
+		<dialog class={styles.fileModal} open={Boolean(getFsItemOpenedInModal())}>
+			<header>
+				<h2>{getItemName()}</h2>
+				<ModalCloseButton onClick={closeModal}>X</ModalCloseButton>
+			</header>
+			<main>
+				<div class={styles.preview}>
+					тут будет превью
+					и метаданные
+				</div>
+
+				<section class={styles.actions}>
+					<span>Name: {getItemName()}</span>
+					<span>Path: {getItemPath()}</span>
+				</section>
+			</main>
+		</dialog>
+	)
+}
+
+export default FileModal
