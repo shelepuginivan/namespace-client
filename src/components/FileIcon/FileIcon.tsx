f99fb69fa67e1e4db8197d06e015407ff05554df
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 14:16:31 +0300

    added modal open on right click

--- src/components/FileIcon/FileIcon.tsx
+++ src/components/FileIcon/FileIcon.tsx
@@ -1,14 +1,22 @@
 import {JSX} from 'solid-js'
 import currentWorkingDirectory from '../../store/currentWorkingDirectory'
 import styles from './FileIcon.module.css'
-import {IFileSystemItem} from '../../utils/interfaces/IFileSystemItem'
 import fileTypesIcons from '../../utils/fileTypesIcons'
+import fsItemOpenedInModal from '../../store/fsItemOpenedInModal'
+import FileSystemItem from '../../utils/FileSystemItem'
+import {IFileSystemItem} from '../../utils/interfaces/IFileSystemItem'
 
 const FileIcon = (props: IFileSystemItem): JSX.Element => {
 	const setCWD = currentWorkingDirectory[1]
+	const setFsItemOpenedInModal = fsItemOpenedInModal[1]
 
 	const doubleClickHandler = () => {
 		if (props.isDirectory) return setCWD(props.path)
+	}
+
+	const rightClickHandler = (e: Event) => {
+		e.preventDefault()
+		setFsItemOpenedInModal(new FileSystemItem(props))
 	}
 
 
@@ -16,6 +24,7 @@ const FileIcon = (props: IFileSystemItem): JSX.Element => {
 		<div
 			class={styles.fileIcon}
 			ondblclick={doubleClickHandler}
+			oncontextmenu={rightClickHandler}
 		>
 			<img
 				src={fileTypesIcons[props.isDirectory ? 'folder' : props.extension.replace('.', '')]}
