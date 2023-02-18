fc18de4f2bf74cb895f3aa933aee3234b7fe497d
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-19 02:48:50 +0300

    imported svg font

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import { render } from 'solid-js/web'
 import App from './components/App'
 import './css/reset.css'
 import './css/variables.css'
+import './assets/fonts/icomoon/style.css'
 
 render(() => <App />, document.getElementById('root'))
