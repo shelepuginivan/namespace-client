3388c8fc3bd77262274ef38ba29ba816e01b746b
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 10:28:36 +0300

    added ui component FilesList

---
+++ src/ui/FilesList/FilesList.tsx
@@ -0,0 +1,12 @@
+import {JSX, ParentProps} from 'solid-js'
+import styles from './FilesList.module.css'
+
+const FilesList = (props: ParentProps): JSX.Element => {
+	return (
+		<div class={styles.filesList}>
+			{props.children}
+		</div>
+	)
+}
+
+export default FilesList
