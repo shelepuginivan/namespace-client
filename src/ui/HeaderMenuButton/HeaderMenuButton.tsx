9066938bccfbbed781ae11768f1199b9bbd72818
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 12:58:42 +0300

    added ui component HeaderMenuButton

---
+++ src/ui/HeaderMenuButton/HeaderMenuButton.tsx
@@ -0,0 +1,12 @@
+import {JSX} from 'solid-js'
+import styles from './HeaderMenuButton.module.css'
+
+const HeaderMenuButton = (props: JSX.ButtonHTMLAttributes<any>): JSX.Element => {
+	const {children, ...buttonProps} = props
+
+	return (
+		<button class={styles.button} {...buttonProps}>{children}</button>
+	)
+}
+
+export default HeaderMenuButton
