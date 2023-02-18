2a437bf53505bfe99507a21ad0ef65fa6d22cdd3
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 11:12:16 +0300

    added HeaderBar

--- src/pages/FileSystemPage.tsx
+++ src/pages/FileSystemPage.tsx
@@ -1,22 +1,31 @@
 import {JSX} from 'solid-js'
-import itemsInCurrentWorkingDirectory from "../store/itemsInCurrentWorkingDirectory";
-import FileIcon from "../ui/FileIcon/FileIcon";
-import FilesList from "../ui/FilesList/FilesList";
+import itemsInCurrentWorkingDirectory from '../store/itemsInCurrentWorkingDirectory'
+import currentWorkingDirectory from '../store/currentWorkingDirectory'
+import connectionURL from '../store/connectionURL'
+import FileIcon from '../ui/FileIcon/FileIcon'
+import FilesList from '../ui/FilesList/FilesList'
+import styles from './Page.module.css'
+import HeaderBar from '../ui/HeaderBar/HeaderBar'
 
 const FileSystemPage = (): JSX.Element => {
-	const [getItemsInCurrentWorkingDirectory, setItemsInCurrentWorkingDirectory] = itemsInCurrentWorkingDirectory
+	const getItemsInCurrentWorkingDirectory = itemsInCurrentWorkingDirectory[0]
+	const getCWD = currentWorkingDirectory[0]
+	const getConnectionURL = connectionURL[0]
 
 	return (
-		<FilesList>
-			{getItemsInCurrentWorkingDirectory().map(item => (
-				<FileIcon
-					path={item.path}
-					extension={item.extension}
-					isDirectory={item.isDirectory}
-					name={item.name}
-				/>
-			))}
-		</FilesList>
+		<div class={styles.page}>
+			<HeaderBar cwd={getCWD()} host={getConnectionURL()}></HeaderBar>
+			<FilesList>
+				{getItemsInCurrentWorkingDirectory().map(item => (
+					<FileIcon
+						path={item.path}
+						extension={item.extension}
+						isDirectory={item.isDirectory}
+						name={item.name}
+					/>
+				))}
+			</FilesList>
+		</div>
 	)
 }
 
