3aeddc971d3376f762747b5e5fce7169f8a47739
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 12:58:25 +0300

    added component HeaderMenu

---
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -0,0 +1,22 @@
+import {JSX} from 'solid-js'
+import currentWorkingDirectory from '../../store/currentWorkingDirectory'
+import HeaderMenuButton from '../../ui/HeaderMenuButton/HeaderMenuButton'
+import styles from './HeaderMenu.module.css'
+
+const HeaderMenu = (): JSX.Element => {
+	const [getCWD, setCWD] = currentWorkingDirectory
+
+	const back = () => {
+		const backDir: string[] = getCWD().split('/')
+		backDir.pop()
+		setCWD(backDir.join('/') || '/')
+	}
+
+	return (
+		<menu class={styles.menu}>
+			<HeaderMenuButton onclick={back}>..</HeaderMenuButton>
+		</menu>
+	)
+}
+
+export default HeaderMenu
