6384ac6ac5bc82ec808cc251fc6280ce1dc53c9e
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 12:13:40 +0300

    added double click handler

--- src/components/FileIcon/FileIcon.tsx
+++ src/components/FileIcon/FileIcon.tsx
@@ -1,11 +1,22 @@
 import {JSX} from 'solid-js'
+import currentWorkingDirectory from '../../store/currentWorkingDirectory'
 import styles from './FileIcon.module.css'
 import {IFileSystemItem} from '../../utils/interfaces/IFileSystemItem'
 import fileTypesIcons from '../../utils/fileTypesIcons'
 
 const FileIcon = (props: IFileSystemItem): JSX.Element => {
+	const setCWD = currentWorkingDirectory[1]
+
+	const doubleClickHandler = () => {
+		if (props.isDirectory) return setCWD(props.path)
+	}
+
+
 	return (
-		<div class={styles.fileIcon}>
+		<div
+			class={styles.fileIcon}
+			ondblclick={doubleClickHandler}
+		>
 			<img
 				src={fileTypesIcons[props.isDirectory ? 'folder' : props.extension.replace('.', '')]}
 				alt={props.extension}
