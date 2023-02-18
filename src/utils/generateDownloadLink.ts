9da12b198879b2532e97052602a10bd66b0af561
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 14:51:44 +0300

    added function for generating download links

---
+++ src/utils/generateDownloadLink.ts
@@ -0,0 +1,6 @@
+import connectionURL from '../store/connectionURL'
+
+export const generateDownloadLink = (pathToFile: string): string => {
+	const getConnectionURL = connectionURL[0]
+	return `${getConnectionURL()}files${pathToFile}`
+}
