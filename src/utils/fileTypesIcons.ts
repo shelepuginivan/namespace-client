68368ec1d523310baa26bb9f46656057e37bbd03
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 10:23:02 +0300

    added object for accessing icons by extension

---
+++ src/utils/fileTypesIcons.ts
@@ -0,0 +1,13 @@
+import folder from '../assets/ext-icons/folder.svg'
+import txt from '../assets/ext-icons/txt.svg'
+import pdf from '../assets/ext-icons/pdf.svg'
+import jpg from '../assets/ext-icons/jpg.svg'
+import mp3 from '../assets/ext-icons/mp3.svg'
+
+export default {
+	folder,
+	txt,
+	pdf,
+	jpg,
+	mp3
+}
