9744c761c71752b4b4638c65d68d6f9eb53e806d
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-19 10:25:55 +0300

    implemented drag and drop file upload

--- src/pages/FileSystemPage.tsx
+++ src/pages/FileSystemPage.tsx
@@ -2,23 +2,57 @@ import {JSX} from 'solid-js'
 import itemsInCurrentWorkingDirectory from '../store/itemsInCurrentWorkingDirectory'
 import currentWorkingDirectory from '../store/currentWorkingDirectory'
 import connectionURL from '../store/connectionURL'
+import SocketioClient from "../store/socketioClient";
 import FileIcon from '../components/FileIcon/FileIcon'
-import FilesList from '../ui/FilesList/FilesList'
+import FilesList from '../components/FilesList/FilesList'
 import styles from './Page.module.css'
 import HeaderBar from '../ui/HeaderBar/HeaderBar'
 import HeaderMenu from '../components/HeaderMenu/HeaderMenu'
+import ky from 'ky'
 
 const FileSystemPage = (): JSX.Element => {
+	const getSocketioClient = SocketioClient[0]
 	const getItemsInCurrentWorkingDirectory = itemsInCurrentWorkingDirectory[0]
 	const getCWD = currentWorkingDirectory[0]
 	const getConnectionURL = connectionURL[0]
+
+	const dragCommonHandler = (e: Event) => {
+		e.preventDefault()
+		e.stopPropagation()
+	}
+
+	const dropHandler = async (e: Event) => {
+		e.preventDefault()
+		e.stopPropagation()
+
+		const files: FileList = (e as DragEvent).dataTransfer.files
+		console.log(files)
+		const fileUploadBody = new FormData()
+
+		for (let i = 0; i < files.length; i++) {
+			fileUploadBody.append(`${getCWD()}/${files[i].name}`, files[i])
+		}
+
+		try {
+			await ky.post(`${getConnectionURL()}/files`, {body: fileUploadBody})
+			getSocketioClient().emit('updateFiles', getCWD())
+		} catch (e) {
+
+		}
+	}
 
 	return (
 		<div class={styles.page}>
 			<HeaderBar cwd={getCWD()} host={getConnectionURL()}></HeaderBar>
 			<div>
 				<HeaderMenu/>
-				<FilesList>
+				<FilesList
+					ondragenter={dragCommonHandler}
+					ondragover={dragCommonHandler}
+					ondragend={dragCommonHandler}
+					ondragleave={dragCommonHandler}
+					ondrop={dropHandler}
+				>
 					{getItemsInCurrentWorkingDirectory().map(item => (
 						<FileIcon {...item} />
 					))}
