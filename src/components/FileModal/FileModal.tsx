f024a1206b6eaa7e7f2c15c4598e372be2be5e7f
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-21 20:55:56 +0300

    implemented file system item rename option

--- src/components/FileModal/FileModal.tsx
+++ src/components/FileModal/FileModal.tsx
@@ -1,21 +1,25 @@
-import {createEffect, createSignal, JSX} from 'solid-js'
+import {createEffect, createSignal, JSX, Show} from 'solid-js'
 
+import currentWorkingDirectory from '../../store/currentWorkingDirectory'
 import fsItemOpenedInModal from '../../store/fsItemOpenedInModal'
 import socketioClient from '../../store/socketioClient'
-import ModalCloseButton from '../../ui/ModalCloseButton/ModalCloseButton'
 import FileSystemItem from '../../utils/FileSystemItem'
 import FSItemParser from '../../utils/FSItemParser'
 import {generateLink} from '../../utils/generateLink'
 import Preview from '../Preview/Preview'
 import styles from './FileModal.module.css'
+import Input from '../../ui/Input/Input'
 
 const FileModal = (): JSX.Element => {
 	const getSocketioClient = socketioClient[0]
+	const getCWD = currentWorkingDirectory[0]
 	const [getFsItemOpenedInModal, setFsItemOpenedInModal] = fsItemOpenedInModal
 	const [getItemName, setItemName] = createSignal<string>('')
 	const [getItemPath, setItemPath] = createSignal<string>('')
 	const [getItemDescription, setItemDescription] = createSignal<string>('')
 	const [getItemSize, setItemSize] = createSignal<string>('')
+	const [getInRenameMode, setInRenameMode] = createSignal<boolean>(false)
+	const [getNewName, setNewName] = createSignal<string>('')
 
 	createEffect(() => {
 		const openedFile = getFsItemOpenedInModal()
@@ -26,10 +30,12 @@ const FileModal = (): JSX.Element => {
 			setItemPath(openedFile.path)
 			setItemDescription(FSItemParser.getItemDescription(openedFile))
 			setItemSize(FSItemParser.getReadableSize(openedFile))
+			setNewName(openedFile.name)
 		}
 	})
 
 	const closeModal = () => {
+		setInRenameMode(false)
 		setFsItemOpenedInModal(null)
 	}
 
@@ -41,11 +47,22 @@ const FileModal = (): JSX.Element => {
 		}
 	}
 
+	const renameItem = () => {
+		try {
+			const itemToRename = `${getCWD()}/${getItemName()}`
+			const newName = `${getCWD()}/${getNewName()}`
+
+			getSocketioClient()?.emit('renameItem', itemToRename, newName)
+		} finally {
+			closeModal()
+		}
+	}
+
 	return (
 		<dialog class={styles.fileModal} open={Boolean(getFsItemOpenedInModal())}>
 			<header>
 				<h2>{getItemName()}</h2>
-				<ModalCloseButton onClick={closeModal}>X</ModalCloseButton>
+				<i class={`icon-close ${styles.closeButton}`} onclick={closeModal}></i>
 			</header>
 			<main>
 				<Preview/>
@@ -61,8 +78,25 @@ const FileModal = (): JSX.Element => {
 					</table>
 
 					<div class={styles.actions}>
-						<a target="_blank" class={styles.action} href={generateLink(getItemPath())}><span class="icon-download"></span>Скачать</a>
-						<button class={styles.action} onclick={deleteItem}><span class="icon-delete"></span> Удалить</button>
+						<Show keyed when={!getInRenameMode()} fallback={
+							<div>
+								<Input
+									onchange={e => setNewName((e.target as HTMLInputElement).value)} type="text"
+									value={getNewName()}
+								/>
+								<button onclick={() => setInRenameMode(false)}>Отмена</button>
+								<button onclick={renameItem}>Сохранить</button>
+							</div>
+						}>
+							<button class={styles.action} onClick={() => setInRenameMode(true)}>
+								<span class="icon-rename"></span> Переименовать
+							</button>
+							<a target="_blank" class={styles.action} href={generateLink(getItemPath())}><span
+								class="icon-download"></span>Скачать</a>
+							<button class={styles.action} onClick={deleteItem}><span
+								class="icon-delete"></span> Удалить
+							</button>
+						</Show>
 					</div>
 				</section>
 			</main>
