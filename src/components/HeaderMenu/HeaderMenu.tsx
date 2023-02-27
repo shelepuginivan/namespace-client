9d05461418099e59555f1f6ed376b84750f801b9
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-27 23:41:26 +0300

    used `backDirectory` function

--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -4,21 +4,16 @@ import currentWorkingDirectory from '../../store/currentWorkingDirectory'
 import showFileIcons from '../../store/showFileIcons'
 import HeaderMenuButton from '../../ui/HeaderMenuButton/HeaderMenuButton'
 import styles from './HeaderMenu.module.css'
+import {backDirectory} from '../../utils/backDirectory'
 
 const HeaderMenu = (): JSX.Element => {
-	const [getCWD, setCWD] = currentWorkingDirectory
+	const setCWD = currentWorkingDirectory[1]
 	const setShowFileIcons = showFileIcons[1]
-
-	const back = () => {
-		const backDir: string[] = getCWD().split('/')
-		backDir.pop()
-		setCWD(backDir.join('/') || '/')
-	}
 
 	return (
 		<menu class={styles.menu}>
 			<HeaderMenuButton onclick={() => setCWD('/')}><i class="icon-home"></i></HeaderMenuButton>
-			<HeaderMenuButton onclick={back}><i class="icon-back"></i></HeaderMenuButton>
+			<HeaderMenuButton onclick={() => setCWD(prev => backDirectory(prev))}><i class="icon-back"></i></HeaderMenuButton>
 			<HeaderMenuButton onclick={() => setShowFileIcons(false)}><i class="icon-list"></i></HeaderMenuButton>
 			<HeaderMenuButton onclick={() => setShowFileIcons(true)}><i class="icon-table"></i></HeaderMenuButton>
 		</menu>
