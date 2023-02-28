d45ca828e8d319d1597002fc4cf6197239112154
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-01 02:04:22 +0300

    added ui component `SubmitForm`

---
+++ src/ui/SubmitForm/SubmitForm.tsx
@@ -0,0 +1,12 @@
+import {JSX} from 'solid-js'
+import styles from './SubmitForm.module.css'
+
+type PropsType = Omit<JSX.InputHTMLAttributes<HTMLInputElement>, 'type'>
+
+const SubmitForm = (props: PropsType) => {
+	return (
+		<input class={styles.submitForm} type="submit" {...props}/>
+	)
+}
+
+export default SubmitForm
