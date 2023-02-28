972b94610c737239fe747d90b983d2375c217f00
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-01 02:04:31 +0300

    added ui component `AuthInput`

---
+++ src/ui/AuthInput/AuthInput.tsx
@@ -0,0 +1,19 @@
+import {JSX} from 'solid-js'
+import styles from './AuthInput.module.css'
+
+type PropsType = JSX.InputHTMLAttributes<HTMLInputElement> & {
+	label: string
+}
+
+const AuthInput = (props: PropsType): JSX.Element => {
+	const {id, label, ...inputProps} = props
+
+	return (
+		<div class={styles.inputWrapper}>
+			<label for={id}>{label}</label>
+			<input id={id} {...inputProps}/>
+		</div>
+	)
+}
+
+export default AuthInput
