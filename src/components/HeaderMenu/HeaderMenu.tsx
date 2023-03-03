25d94e9686c8d5d29bf3408b70ea6887d90fac2a
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-03 20:39:36 +0300

    added create directory button

--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -2,13 +2,19 @@ import {JSX} from 'solid-js'
 
 import currentWorkingDirectory from '../../store/currentWorkingDirectory'
 import showFileIcons from '../../store/showFileIcons'
+import socketioClient from '../../store/socketioClient'
 import HeaderMenuButton from '../../ui/HeaderMenuButton/HeaderMenuButton'
 import styles from './HeaderMenu.module.css'
 import {backDirectory} from '../../utils/backDirectory'
 
 const HeaderMenu = (): JSX.Element => {
-	const setCWD = currentWorkingDirectory[1]
+	const getSocketioClient = socketioClient[0]
+	const [getCWD, setCWD] = currentWorkingDirectory
 	const setShowFileIcons = showFileIcons[1]
+
+	const createDirectory = () => {
+		getSocketioClient().emit('createDirectory', `${getCWD()}/Новая папка`)
+	}
 
 	return (
 		<menu class={styles.menu}>
@@ -16,6 +22,7 @@ const HeaderMenu = (): JSX.Element => {
 			<HeaderMenuButton onclick={() => setCWD(prev => backDirectory(prev))}><i class="icon-back"></i></HeaderMenuButton>
 			<HeaderMenuButton onclick={() => setShowFileIcons(false)}><i class="icon-list"></i></HeaderMenuButton>
 			<HeaderMenuButton onclick={() => setShowFileIcons(true)}><i class="icon-table"></i></HeaderMenuButton>
+			<HeaderMenuButton onclick={createDirectory}>Создать папку</HeaderMenuButton>
 		</menu>
 	)
 }
