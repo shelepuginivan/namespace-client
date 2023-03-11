c128e7e5212fec86e7d531d085c667a2217d9476
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-11 22:02:14 +0300

    fixed code styles

--- src/utils/FileData.ts
+++ src/utils/FileData.ts
@@ -15,13 +15,13 @@ import url from '../assets/fileExtensionIcons/url.svg'
 import vim from '../assets/fileExtensionIcons/vim.svg'
 import xml from '../assets/fileExtensionIcons/xml.svg'
 import archive from '../assets/fileTypeIcons/archive.svg'
+import audio from '../assets/fileTypeIcons/audio.svg'
+import defaultFile from '../assets/fileTypeIcons/defaultFile.svg'
+import folder from '../assets/fileTypeIcons/folder.svg'
 import image from '../assets/fileTypeIcons/image.svg'
+import video from '../assets/fileTypeIcons/video.svg'
 import FileSystemItem from './FileSystemItem'
 import {IFileData} from './interfaces/IFileData'
-import folder from '../assets/fileTypeIcons/folder.svg'
-import audio from '../assets/fileTypeIcons/audio.svg'
-import video from '../assets/fileTypeIcons/video.svg'
-import defaultFile from '../assets/fileTypeIcons/defaultFile.svg'
 
 export class FileData implements IFileData {
 	private static _instances: FileData[] = []
