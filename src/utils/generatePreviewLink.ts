4718aec5483b22c431c5e5b39de65b49b9bc96b4
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-12 15:06:03 +0300

    removed replaced functions

--- src/utils/generatePreviewLink.ts
+++
@@ -1,6 +0,0 @@
-import connectionURL from '../store/connectionURL'
-
-export const generatePreviewLink = (pathToFile: string): string => {
-	const getConnectionURL = connectionURL[0]
-	return `${getConnectionURL()}/files/preview?path=${pathToFile}`
-}
