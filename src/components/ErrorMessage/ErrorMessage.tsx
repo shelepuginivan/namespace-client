dfeef3bd337837adf8019a9b5610b3fcb3abf5f4
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-16 20:14:49 +0300

    Added ErrorMessage modal

---
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,20 @@
+import {JSX} from 'solid-js'
+import styles from './errorMessage.module.css'
+import currentSocketError from '../../store/currentSocketError'
+import {errorType} from '../../utils/constants'
+
+const ErrorMessage = (): JSX.Element => {
+	const getSocketError = currentSocketError[0]
+
+	return (
+		<dialog
+			class={styles.errorMessage}
+			open={Boolean(getSocketError()?.message)}
+		>
+			<h4>{errorType[getSocketError()?.type]}</h4>
+			<p>{getSocketError()?.message}</p>
+		</dialog>
+	)
+}
+
+export default ErrorMessage
